a31f1847d77679cdaeec2568ec12652973bc065f
Lucas Miño <65635859+Lucasgm2020@users.noreply.github.com>
2022-02-02 17:42:31 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,232 +1,247 @@
 from tkinter import *
-from tkinter import messagebox, filedialog
 import sqlite3
 import os
+from tkinter import messagebox
 
-
-
+path = str(os.getcwd()) + r"\bbdd.db" 
 root = Tk()
-path = str(os.getcwd())+r"\bbdd.db"
+caso_guardar = "nuevo"
 
-#Funciones
-def deshabilitar_campos():
-    entry_duracion.config(state="disabled")
-    entry_genero.config(state="disabled")
-    entry_nombre.config(state="disabled")
-    var_duracion.set("")
-    var_nombre.set("")
-    var_genero.set("")
+#2) Funciones de habilitación de botones
 
-def deshabilitar_botones(btn_nuevo=False):
-    if btn_nuevo:
-        button_nuevo.config(state="disabled")
+def deshabilitar(caso=""):
+    if caso == "todo":
         button_editar.config(state="disabled")
+        button_nuevo.config(state="disabled")
         button_eliminar.config(state="disabled")
-    button_cancelar.config(state="disabled")
     button_guardar.config(state="disabled")
-
-
-def habilitar_campos():
-    entry_duracion.config(state="normal")
-    entry_genero.config(state="normal")
-    entry_nombre.config(state="normal")
-
-def habilitar_botones(parameter):
-    if parameter == 0:
-        button_nuevo.config(state="normal")
+    button_cancelar.config(state="disabled")
+    entry_nombre.config(state="disabled")
+    entry_duración.config(state="disabled")
+    entry_genero.config(state="disabled")   
+    var_nombre.set("")
+    var_duracion.set("")
+    var_genero.set("")
+    
+    
+def habilitar_botones(caso):
+    if caso == "BBDD":
         button_editar.config(state="normal")
+        button_nuevo.config(state="normal")
         button_eliminar.config(state="normal")
-    elif parameter ==1:
+    elif caso == "nuevo" or "editar":
         button_cancelar.config(state="normal")
         button_guardar.config(state="normal")
+        entry_nombre.config(state="normal")
+        entry_duración.config(state="normal")
+        entry_genero.config(state="normal")  
 
-def habilitar_nuevo():
-    habilitar_campos()
-    habilitar_botones(1)
-
-def cancelar():
-    deshabilitar_campos()
-    deshabilitar_botones()
-
-def run_query(path,query,parameters=()):
+def run_query(query,parameters=()):
     with sqlite3.connect(path) as bd:
         cursor = bd.cursor()
-        cursor.execute(query,parameters)        
-        bd.commit() #guarda la consulta/accion realizada (si corresponde) en la base de datos
-        resultado = cursor.fetchall()
-    return resultado
+        cursor.execute(query,parameters)
+        bd.commit()#guarda la consulta/accion realizada (si corresponde) en la base de datos
+        results = cursor.fetchall() #retorna lo seleccionado si corresponde.
+    return results
 
-def guardar_registro():
-    datos = [var_nombre.get(),var_duracion.get(),var_genero.get()]
-    respuesta = True
-    if "" in datos:
-        respuesta = messagebox.askyesno(message="Uno de los campos está vacío, ¿desea guardar los datos igualmente?")
-    if respuesta:
-        try:
-            query = "INSERT INTO DATOS VALUES(NULL,?,?,?)"
-            run_query(path,query,datos)
-            actualizar_datos()
-            deshabilitar_campos()
-            deshabilitar_botones()
-            messagebox.showinfo("Ok",f"""\
-            El registro:
-            
-            NOMBRE = {datos[0]}
-            DURACION = {datos[1]}
-            GENERO = {datos[2]}
-            
-            se ha guardado correctamente.""")
-            
-        except Exception as e:
-            messagebox.showerror("Error",str(e))
-       
-
-def conectar_bbdd():
+def conectar_BBDD():
     try:
-        conexion=sqlite3.connect(path)    
-        cursor=conexion.cursor()
         query = "SELECT * FROM DATOS"
-        run_query(path,query)
-        messagebox.showinfo("Conexion","Se ha conectado a la base de datos correctamente")
-        
-        habilitar_botones(0)
-    except Exception as e:
-        messagebox.showerror("Error","Se ha producido el siguiente error en la conexion a la base de datos: \n\n" + str(e).upper())
-        respuesta = messagebox.askyesno(title="Conexion",message="¿Desea crear una nueva tabla?")
-        if respuesta:
+        run_query(query)
+        mensaje(titulo="Conexion",msj="Se ha conectado con la base de datos correctamente")
+        mostrar_registros()
+        habilitar_botones("BBDD")
+    except Exception as error:
+        mensaje(titulo="Error",error=str(error))
+        result = messagebox.askyesno("Crear BBDD","¿Desea crear una nueva tabla en la BBDD?")
+        if result:
             try:
-                query="CREATE TABLE DATOS('ID' INTEGER PRIMARY KEY AUTOINCREMENT,'NAME' VARCHAR(70),'DURACION' VARCHAR(5),'GENERO' VARCHAR(40))"
-                run_query(path,query)
-                messagebox.showinfo("Conexion","Base de datos creada correctamente")
-                habilitar_botones(0)
-            except Exception as e:
-                messagebox.showerror("Error","Se ha producido el siguiente error en la creacion de la base de datos: \n\n" + str(e).upper())
-    conexion.close()
-    actualizar_datos()
+                query = "CREATE TABLE DATOS ('ID' INTEGER PRIMARY KEY AUTOINCREMENT,'NOMBRE' TEXT VARCHAR(50),'DURACIÓN' REAL,'GÉNERO' TEXT VARCHAR(50))"
+                run_query(query)
+                mensaje(titulo="Conexion",msj="Se ha creado correctamente la base de datos.")
+                mostrar_registros()
+                habilitar_botones("BBDD")
+            except Exception as error:   
+                mensaje(titulo="Error",error=str(error))
 
-def editar_registro():
-    pass
-#     if tree.selection() == "":
-#         messagebox.showwarning(title="Selection",message="No se ha seleccionado ninguna fila.")
-#     else:
-#         habilitar_nuevo()
-#         
-#             
-#         datos = [var_nombre.get(),var_duracion.get(),var_genero.get()]
-#         respuesta = True
-#         if "" in datos:
-#             respuesta = messagebox.askyesno(message="Uno de los campos está vacío, ¿desea actualizar los datos igualmente?")
-#         if respuesta:
-#             try:
-#                 query = f"UPDATE DATOS set NAME = ?, DURACION = ?, GENERO = ? WHERE NAME = {datos[0]}, DURACION = {datos[1]} AND GENERO = {datos[2]}"
-#                 run_query(path,query,datos)
-#                 actualizar_datos()
-#                 deshabilitar_campos()
-#                 
-#             except Exception as e:
-#                 messagebox.showerror("Error",str(e))  
+
+def mostrar_registros():
+    query = "SELECT * FROM DATOS ORDER BY ID DESC"
+    resultados = run_query(query)
     
-def borrar_tabla():
+    borrar_registros()
+    
+    for resultado in resultados:
+        tree.insert("",0,text=resultado[0],values=[resultado[1],resultado[2],resultado[3]])
+        
+
+def borrar_registros():
+    registros = tree.get_children()
+
+    for registro in registros:
+        tree.delete(registro)   
+
+def nuevo_registro():
+    habilitar_botones("nuevo")
+    global caso_guardar
+    caso_guardar = "nuevo"
+    
+def guardar_registro(caso=caso_guardar):
+    try:
+        if caso_guardar == "nuevo":
+            query = "INSERT INTO DATOS VALUES (NULL,?,?,?)"
+            values = [var_nombre.get(),var_duracion.get(),var_genero.get()]
+            run_query(query,values)
+            borrar_registros()
+            mostrar_registros()          
+            mensaje(titulo="Ok",msj="Se ha agregado el registro correctamente.")
+            
+        elif caso_guardar == "editar":
+            values = [var_nombre.get(),var_duracion.get(),var_genero.get()]
+            query = f"UPDATE DATOS set NOMBRE=?,DURACIÓN=?,GÉNERO=? WHERE NOMBRE='{str(var_nombre.get())}'" 
+            run_query(query,values)
+            borrar_registros()
+            mostrar_registros()
+            mensaje(titulo="Ok",msj="Se ha editado correctamente el registro.")
+            
+    except Exception as error: 
+        mensaje(titulo="Error",error="Error")
+    deshabilitar()
+    
+def cancelar():
+    deshabilitar()
+
+def editar():
+    try:
+
+        data = selectedItem() 
+        nombre,duracion,genero = data[0],data[1],data[2]
+        values = [nombre,duracion,genero]
+        var_nombre.set(nombre)
+        var_duracion.set(duracion)
+        var_genero.set(genero)
+        global caso_guardar
+        caso_guardar = "editar"
+        habilitar_botones("editar")
+    except Exception as error:
+        print(str(error))
+        if str(error)=="string index out of range":
+            error = "No se ha seleccionado un registro."
+        mensaje(titulo="Error",error=str(error))
+        deshabilitar()
+        
+def selectedItem():
+    try:
+        curItem = tree.focus()
+        return(tree.item(curItem)["values"])
+    except Exception as error:
+        mensaje(titulo="Error",error=str(error))
+        
+def mensaje(titulo,error="",msj="Se ha producido el siguiente error: \n\n"):
+    if len(str(error))==0:
+        messagebox.showinfo(titulo,msj)
+    else:
+        messagebox.showerror(titulo,msj + error.upper())
+        
+def eliminar_registro():
+    try:
+        data = selectedItem()
+        query = f"DELETE FROM DATOS WHERE NOMBRE='{data[0]}'"
+        run_query(query)
+        mensaje(titulo="Ok",msj=f"El siguiente registro se ha borrado correctamente:\n\nNombre={data[0]}\nDuración={data[1]}\nGenero={data[2]}")
+        deshabilitar()
+        borrar_registros()
+        mostrar_registros()
+    except Exception as error:
+        if str(error)=="string index out of range":
+            error = "No se ha seleccionado un registro."
+        mensaje(titulo="Error",error=str(error))    
+        deshabilitar()
+ 
+def borrar_BBDD():
     query = "DROP TABLE DATOS"
     try:
-        run_query(path,query)
-        messagebox.showinfo("Borrar","Se ha borrado la base de datos correctamente")
-        borrar_datos()
-        deshabilitar_campos()
-        deshabilitar_botones(True)
-    except Exception as e:
-        messagebox.showerror("Error",e)
-
-def borrar_datos():
-    records = tree.get_children()
-    for record in records:
-        tree.delete(record)
-
-def actualizar_datos():
-    try:
-        query = "SELECT * FROM DATOS ORDER BY ID DESC "
-        db_rows = run_query(path,query)
-        records = tree.get_children()
-    
-        for record in records:
-            tree.delete(record)
-         
-        for row in db_rows:
-            tree.insert("",0,text=row[0],values=[row[1],row[2],row[3]])
+        run_query(query)
+        mensaje(titulo="Borrar",msj="Se ha borrado correctamente la BBDD")
+        borrar_registros()
+        deshabilitar("todo")
+    except Exception as error:
+        mensaje(titulo="Error",error=str(error))
         
-    except:
-        pass
-    
-#MENU
+#1) Apariencia de la aplicación
 
-menu = Menu(root)
-inicio_menu = Menu(menu,tearoff=0)
+#Frames
 
-inicio_menu.add_command(label="Conectar con DB",command=conectar_bbdd)
-inicio_menu.add_command(label="Eliminar registro en DB",command=borrar_tabla)
-inicio_menu.add_command(label="Salir",command = lambda: root.destroy())
-menu.add_cascade(label="Inicio",menu=inicio_menu)
+Frame(root,width=800,height=250).grid(row=0,column=0,rowspan=4,columnspan=3)
+Frame(root,width=800,height=200).grid(row=4,column=0,rowspan=1,columnspan=4)
+Frame(root,width=800,height=75).grid(row=5,column=0,rowspan=1,columnspan=3)
 
-menu.add_cascade(label="Consultas")
+#Labels
+Label(root,text="Nombre",font=("arial",10,"bold"),fg="black").grid(row=0,column=0)
+Label(root,text="Duración",font=("arial",10,"bold"),fg="black").grid(row=1,column=0)
+Label(root,text="Genero",font=("arial",10,"bold"),fg="black").grid(row=2,column=0)
 
-menu.add_cascade(label="Configuracion")
-
-menu.add_cascade(label="Ayuda")
-
-#FRAMES
-frm_entries = Frame(root,height=250,width=800)
-frm_entries.grid(row=0,column=0,rowspan=4,columnspan=3,pady=0)
-
-frm_treeview = Frame(root,height=200,width=800)
-frm_treeview.grid(row=4,column=0,columnspan=4)
-
-frm_buttons = Frame(root,height=75,width=800)
-frm_buttons.grid(row=5,column=0,columnspan=3)
-
-#LABELS
-Label(root,text="Nombre: ",font=("arial",10,"bold")).grid(row=0,column=0)
-Label(root,text="Duracion: ",font=("arial",10,"bold")).grid(row=1,column=0)
-Label(root,text="Género: ",font=("arial",10,"bold")).grid(row=2,column=0)
-
-#ENTRIES
-var_nombre = StringVar()
-var_duracion = StringVar()
-var_genero = StringVar()
-entry_nombre = Entry(root,width=75,textvariable=var_nombre)
-entry_nombre.grid(row=0,column=1,columnspan=2,sticky=W)
-entry_duracion = Entry(root,width=75,textvariable=var_duracion)
-entry_duracion.grid(row=1,column=1,columnspan=2,sticky=W)
-entry_genero = Entry(root,width=75,textvariable=var_genero)
-entry_genero.grid(row=2,column=1,columnspan=2,sticky=W)
-
-#BUTTONS
-button_nuevo = Button(root,text="Nuevo",font=("arial",10,"bold"),fg="white",bg="green",width=20,command=habilitar_nuevo)
+#Botones
+button_nuevo = Button(root,text="Nuevo",font=("arial",10,"bold"),fg="white",bg="green",width=20,command=nuevo_registro)
 button_nuevo.grid(row=3,column=0)
 button_guardar = Button(root,text="Guardar",font=("arial",10,"bold"),fg="white",bg="blue",width=20,command=guardar_registro)
 button_guardar.grid(row=3,column=1)
 button_cancelar = Button(root,text="Cancelar",font=("arial",10,"bold"),fg="white",bg="red",width=20,command=cancelar)
 button_cancelar.grid(row=3,column=2)
 
-#TREEVIEW
-tree = ttk.Treeview(root,height=10,column=("NOMBRE","DURACION","GENERO"))
+#Entries
+var_nombre = StringVar()
+var_duracion = StringVar()
+var_genero = StringVar()
+entry_nombre = Entry(root,textvariable=var_nombre,width=75)
+entry_nombre.grid(row=0,column=1,columnspan=2)
+entry_duración = Entry(root,textvariable=var_duracion,width=75)
+entry_duración.grid(row=1,column=1,columnspan=2)
+entry_genero = Entry(root,textvariable=var_genero,width=75)
+entry_genero.grid(row=2,column=1,columnspan=2)
+
+
+#Menu
+menu = Menu(root)
+
+inicio_menu = Menu(menu,tearoff=0)
+inicio_menu.add_command(label="Conectar con BBDD",command=conectar_BBDD)
+inicio_menu.add_command(label="Eliminar BBDD",command=borrar_BBDD)
+inicio_menu.add_command(label="Salir",command=root.destroy)
+menu.add_cascade(label="Inicio",menu=inicio_menu)
+menu.add_cascade(label="Consultas",menu=inicio_menu)
+menu.add_cascade(label="Configuracion",menu=inicio_menu)
+menu.add_cascade(label="Ayuda",menu=inicio_menu)
+
+#Treeview
+tree = ttk.Treeview(root,height=10,column=("NOMBRE","DURACIÓN","GENERO"))
 tree.grid(row=4,column=0,columnspan=4)
-tree.heading("#0",text="ID",anchor=CENTER)
-tree.heading("#1",text="NOMBRE",anchor=CENTER)
-tree.heading("#2",text="DURACION",anchor=CENTER)
-tree.heading("#3",text="GENERO",anchor=CENTER)
+tree.heading("#0",text="ID")
+tree.heading("#1",text="NOMBRE")
+tree.heading("#2",text="DURACIÓN")
+tree.heading("#3",text="GÉNERO")
+
+#Botones de abajo
+
+button_editar = Button(root,text="Editar",font=("arial",10,"bold"),fg="white",bg="green",width=20,command=editar)
+button_editar.grid(row=5,column=0,pady=10,sticky=N)
+button_eliminar = Button(root,text="Eliminar",font=("arial",10,"bold"),fg="white",bg="red",width=20,command=eliminar_registro)
+button_eliminar.grid(row=5,column=1,pady=10,sticky=N)
+
+#---------Estado inicial---------#
+deshabilitar("todo")
 
 
+"""
+Tamaño de la aplicación:
 
-#BUTTONS
-button_editar = Button(root,text="Editar",font=("arial",10,"bold"),fg="white",bg="green",width=20,command=editar_registro)
-button_editar.grid(row=5,column=0,sticky=N,pady=10)
-button_eliminar = Button(root,text="Eliminar",font=("arial",10,"bold"),fg="white",bg="blue",width=20)
-button_eliminar.grid(row=5,column=1,sticky=N,pady=10)  
+Width (total) = 800
+Height (total) = 525
+Distancia del (0,0), desde arriba a la izquierda = 100 + 100
 
-deshabilitar_campos()
-deshabilitar_botones(True)
+"""
 
-root.geometry("800x525+100+100")
-root.resizable(False,False)
+root.resizable(0,0)
 root.config(menu=menu)
+root.geometry("800x525+100+100")
 root.mainloop()
